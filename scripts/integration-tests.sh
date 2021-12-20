dbf48a0c4e84614241afc72db7690c2c8d46b3a8
Benjamin Fuller <benjaminf@vmware.com>
2021-12-20 14:26:13 -0700

    change system-metrics-scraper to have more comprehensive tests (#5)

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -5,6 +5,63 @@ set -eo pipefail
 time=$(date +%s%N)
 cf install-plugin "log-cache" -f
 sleep 120
-cf tail system_metrics_agent -n 1000 --start-time="${time}" | grep system_cpu_core_idle
+cf tail system_metrics_agent -n 1000 --json --start-time="${time}" |
+    jq -r '.batch[]?.gauge.metrics | keys[]?' |
+    sort -u > /tmp/actual-metrics
 
+cat > /tmp/expected-metrics << EOF
+system_cpu_core_idle
+system_cpu_core_sys
+system_cpu_core_user
+system_cpu_core_wait
+system_cpu_idle
+system_cpu_sys
+system_cpu_user
+system_cpu_wait
+system_disk_ephemeral_inode_percent
+system_disk_ephemeral_io_time
+system_disk_ephemeral_percent
+system_disk_ephemeral_read_bytes
+system_disk_ephemeral_read_time
+system_disk_ephemeral_write_bytes
+system_disk_ephemeral_write_time
+system_disk_persistent_inode_percent
+system_disk_persistent_io_time
+system_disk_persistent_percent
+system_disk_persistent_read_bytes
+system_disk_persistent_read_time
+system_disk_persistent_write_bytes
+system_disk_persistent_write_time
+system_disk_system_inode_percent
+system_disk_system_io_time
+system_disk_system_percent
+system_disk_system_read_bytes
+system_disk_system_read_time
+system_disk_system_write_bytes
+system_disk_system_write_time
+system_healthy
+system_load_15m
+system_load_1m
+system_load_5m
+system_mem_kb
+system_mem_percent
+system_network_bytes_received
+system_network_bytes_sent
+system_network_drop_in
+system_network_drop_out
+system_network_error_in
+system_network_error_out
+system_network_ip_forwarding
+system_network_packets_received
+system_network_packets_sent
+system_network_tcp_active_opens
+system_network_tcp_curr_estab
+system_network_tcp_retrans_segs
+system_network_udp_in_errors
+system_network_udp_lite_in_errors
+system_network_udp_no_ports
+system_swap_kb
+system_swap_percent
+EOF
 
+diff /tmp/expected-metrics /tmp/actual-metrics
