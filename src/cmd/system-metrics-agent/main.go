7c6480765e0b52f1c1e0a75e5c92278e40a1ee36
Carson Long <lcarson@vmware.com>
2022-08-02 21:00:12 -0700

    Refactor system metrics agent logging
    Only error/warning output should go to standard error.
    * Put start/stop messages to std out rather than std error.
    * If env variables fail to write we shouldn't error, but we should log
      the error as a warning.
    
    Report envstruct variables to std out.
    
    Only main should exit on expected errors
    * Optionally return an error from LoadConfig and error out from main if
      it occurs.

--- src/cmd/system-metrics-agent/main.go
+++ src/cmd/system-metrics-agent/main.go
@@ -4,18 +4,29 @@ import (
 	"log"
 	"os"
 
-	"code.cloudfoundry.org/system-metrics/pkg/collector"
-
 	"code.cloudfoundry.org/system-metrics/cmd/system-metrics-agent/app"
+	"code.cloudfoundry.org/system-metrics/pkg/collector"
 )
 
+var (
+	stdLogger *log.Logger
+	errLogger *log.Logger
+)
+
+func init() {
+	stdLogger = log.New(os.Stdout, "", log.LstdFlags)
+	errLogger = log.New(os.Stderr, "", log.LstdFlags)
+}
+
 func main() {
-	log := log.New(os.Stderr, "", log.LstdFlags)
-	log.Println("starting system-metrics-agent")
-	defer log.Println("stopping system-metrics-agent")
+	stdLogger.Println("starting system-metrics-agent")
+	defer stdLogger.Println("stopping system-metrics-agent")
 
-	cfg := app.LoadConfig()
+	cfg, err := app.LoadConfig(errLogger)
+	if err != nil {
+		errLogger.Fatalf("failed to load config from environment: %s\n", err)
+	}
 
-	c := collector.New(log)
-	app.NewSystemMetricsAgent(c.Collect, cfg, log).Run()
+	c := collector.New(errLogger)
+	app.NewSystemMetricsAgent(c.Collect, cfg, errLogger).Run()
 }
