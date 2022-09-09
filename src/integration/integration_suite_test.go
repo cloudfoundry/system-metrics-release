af0bfab37c28b442b2ddf5040086b7745d836011
Carson Long <lcarson@vmware.com>
2022-09-09 16:45:09 -0700

    Add integration test suite
    This test suite builds the system metrics agent, runs it, and tests the
    running agent.
    
    The test has path separators built in that differentiate what metrics
    are expected by OS, as the agent emits different metrics for different
    OSes.

---
+++ src/integration/integration_suite_test.go
@@ -0,0 +1,28 @@
+package integration_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
+)
+
+var agentPath string
+
+func TestIntegration(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Integration Test Suite")
+}
+
+var _ = SynchronizedBeforeSuite(func() []byte {
+	// runs on first process
+	path, err := gexec.Build("../cmd/system-metrics-agent")
+	Expect(err).NotTo(HaveOccurred())
+	DeferCleanup(gexec.CleanupBuildArtifacts)
+
+	return []byte(path)
+}, func(path []byte) {
+	// runs on all processes
+	agentPath = string(path)
+})
