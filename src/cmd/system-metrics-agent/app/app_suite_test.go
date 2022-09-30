10fdda15a9dd89623e2d6b1f1f785a7ed4988e9e
Carson Long <lcarson@vmware.com>
2022-09-30 14:52:37 -0700

    Remove unused plumbing package (#43)
    Move the only thing in the plumbing package that was being used, the
    function that generated a TLS client for testing, to the app_test
    package. Delete everything else in the package.

--- src/cmd/system-metrics-agent/app/app_suite_test.go
+++ src/cmd/system-metrics-agent/app/app_suite_test.go
@@ -2,7 +2,12 @@ package app_test
 
 import (
 	"log"
+	"net"
+	"net/http"
 	"testing"
+	"time"
+
+	"code.cloudfoundry.org/tlsconfig"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -12,4 +17,36 @@ func TestApp(t *testing.T) {
 	log.SetOutput(GinkgoWriter)
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "System Metrics Agent App Suite")
+}
+
+func newTLSClient(cert, key, ca, commonName string) *http.Client {
+	tlsConfig, err := tlsconfig.Build(
+		tlsconfig.WithInternalServiceDefaults(),
+		tlsconfig.WithIdentityFromFile(cert, key),
+	).Client(
+		tlsconfig.WithAuthorityFromFile(ca),
+		tlsconfig.WithServerName(commonName),
+	)
+
+	if err != nil {
+		log.Panicf("failed to load API client certificates: %s", err)
+	}
+
+	transport := &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+		TLSClientConfig:       tlsConfig,
+	}
+
+	return &http.Client{
+		Transport: transport,
+	}
 }
--- src/go.mod
+++ src/go.mod
@@ -3,16 +3,12 @@ module code.cloudfoundry.org/system-metrics
 go 1.19
 
 require (
-	code.cloudfoundry.org/go-batching v0.0.0-20171020220229-924d2a9b48ac
 	code.cloudfoundry.org/go-envstruct v1.5.0
-	code.cloudfoundry.org/go-loggregator/v9 v9.0.2
 	code.cloudfoundry.org/tlsconfig v0.0.0-20200131000646-bbe0f8da39b3
 	github.com/onsi/ginkgo/v2 v2.2.0
 	github.com/onsi/gomega v1.20.2
 	github.com/prometheus/client_golang v1.13.0
 	github.com/shirou/gopsutil/v3 v3.22.5
-	google.golang.org/grpc v1.49.0
-	google.golang.org/protobuf v1.28.1
 )
 
 require (
@@ -39,6 +35,6 @@ require (
 	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/tools v0.1.12 // indirect
-	google.golang.org/genproto v0.0.0-20200825200019-8632dd797987 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
