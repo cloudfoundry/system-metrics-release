cloudfoundry/system-metrics-release

commit 308f64ab2245d69c8d9a4e91c1e5317d65ae2f91

update memory calculation to match the bosh-agent (#57)
- Context from bosh engineers:

    Updating System Metrics Agent to use the same memory usage metric that the Bosh Agent uses, that is using MemAvailable over MemUsed. MemAvailable is a more reliable and correct metric than the older hand-calculation that folks do. It is a metric that's maintained by the kernel developers that is intended to tell users how much memory their application can use without causing the system to hit performance issues. This metric was [added back in 2014](https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=34e431b0a) by the kernel developers because the old way of calculating available memory was no longer correct due to changes in how Linux uses and frees memory. Furthermore, the kernel devs expected that more changes to kernel memory management would inevitably come, rendering incorrect any calculations based on what the kernel did in 2014. MemAvailable is a solid estimate of the maximum amount of memory that an application can use without causing performance problems, and is a metric that is updated to remain correct as the internal details of how the kernel manage memory change over time.

Signed-off-by: Ben Fuller <benjaminf@vmware.com>
Signed-off-by: Rebecca Roberts <robertsre@vmware.com>
Signed-off-by: Ben Fuller <benjaminf@vmware.com>

Signed-off-by: Ben Fuller <benjaminf@vmware.com>
Signed-off-by: Rebecca Roberts <robertsre@vmware.com>

src/pkg/collector/collector.go
@@ -205,8 +205,8 @@ func (c *Collector) Collect() (SystemStat, error) {
 		CPUStat:      cpu,
 		CPUCoreStats: coreStats,
 
-		MemKB:      m.Used / 1024,
-		MemPercent: m.UsedPercent,
+		MemKB:      (m.Total - m.Available) / 1024,
+		MemPercent: float64(m.Total-m.Available) / float64(m.Total),
 
 		SwapKB:      s.Used / 1024,
 		SwapPercent: s.UsedPercent,

src/pkg/collector/collector_test.go
@@ -65,8 +65,8 @@ var _ = Describe("Collector", func() {
 		stats, err := c.Collect()
 		Expect(err).ToNot(HaveOccurred())
 
-		Expect(stats.MemKB).To(Equal(uint64(2)))
-		Expect(stats.MemPercent).To(Equal(40.23))
+		Expect(stats.MemKB).To(Equal(uint64(1)))
+		Expect(stats.MemPercent).To(Equal(0.5))
 	})
 
 	It("returns the swap metrics", func() {
@@ -432,8 +432,8 @@ func (s *stubRawCollector) VirtualMemoryWithContext(context.Context) (*mem.Virtu
 	}
 
 	return &mem.VirtualMemoryStat{
-		Used:        2048,
-		UsedPercent: 40.23,
+		Total:     2048,
+		Available: 1024,
 	}, nil
 }
 

commit 68e11b102e189b8b83acf18a879133f1c3639c29

fix MemPercent to be 0-100 instead of 0-1
This error was introduced in 308f64ab2245d69c8d9a4e91c1e5317d65ae2f91
when we updated the free memory algorithm.

Signed-off-by: Matthew Kocher <mkocher@vmware.com>

src/pkg/collector/collector.go
@@ -206,7 +206,7 @@ func (c *Collector) Collect() (SystemStat, error) {
 		CPUCoreStats: coreStats,
 
 		MemKB:      (m.Total - m.Available) / 1024,
-		MemPercent: float64(m.Total-m.Available) / float64(m.Total),
+		MemPercent: float64(m.Total-m.Available) / float64(m.Total) * 100,
 
 		SwapKB:      s.Used / 1024,
 		SwapPercent: s.UsedPercent,

src/pkg/collector/collector_test.go
@@ -66,7 +66,7 @@ var _ = Describe("Collector", func() {
 		Expect(err).ToNot(HaveOccurred())
 
 		Expect(stats.MemKB).To(Equal(uint64(1)))
-		Expect(stats.MemPercent).To(Equal(0.5))
+		Expect(stats.MemPercent).To(Equal(50.0))
 	})
 
 	It("returns the swap metrics", func() {